76d71db9c908c6cbd2825343b14a849719bf0467
BhushanPDS <bhushan.varade@thinkitive.com>
2021-01-27 22:54:39 +0530

    first commit

---
+++ Day12/FilterDemo/src/FiltDemo/MyFilter.java
@@ -0,0 +1,52 @@
+package FiltDemo;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+
+/**
+ * Servlet Filter implementation class MyFilter
+ */
+@WebFilter("/MyFilter")
+public class MyFilter implements Filter {
+
+    
+    public MyFilter() {
+        // TODO Auto-generated constructor stub
+    }
+
+	
+	public void destroy() {
+		// TODO Auto-generated method stub
+	}
+
+	
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+		PrintWriter out=response.getWriter();
+		
+		
+		String user=request.getParameter("username");
+		if(user.length()>=1) {
+			out.println("pre filter......");
+		}
+		else
+		{
+			response.getWriter().println("username cannot be blank <a href='login'>click</a> " );
+		}
+		
+		chain.doFilter(request, response);
+	}
+
+	
+	public void init(FilterConfig fConfig) throws ServletException {
+		// TODO Auto-generated method stub
+	}
+
+}
---
+++ day3/src/Calculator.java
@@ -0,0 +1,20 @@
+
+public class Calculator {
+	
+	public Calculator() {
+		System.out.println("calculator");
+	}
+	public int add(int a,int b)
+	{
+		return a+b;
+	}
+	
+	public int sub(int a,int b)
+	{
+		return a-b;
+	}
+	public int div(int a,int b)
+	{
+		return a/b;
+	}
+}
